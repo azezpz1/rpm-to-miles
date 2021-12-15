11587bc4a9ff0249f33270cc80a6dbe7ba960532
Anthony Panettiere <apanettiere@fastmail.com>
2021-12-14 18:52:25 -0600

    Add default diameter for my bike

--- rpm_to_miles.py
+++ rpm_to_miles.py
@@ -3,7 +3,7 @@ import math
 
 parser = argparse.ArgumentParser(description="Converts indoor cycling RPM to distance")
 parser.add_argument("--rpm", type=int, required=True, help="The average RPM of the ride.")
-parser.add_argument("--diameter", type=int, required=True, help="The diameter of the wheel in inches")
+parser.add_argument("--diameter", type=int, default=18, help="The diameter of the wheel in inches")
 parser.add_argument("--time", type=int, required=True, help="The time spent cycling in minutes")
 
 args = parser.parse_args()
